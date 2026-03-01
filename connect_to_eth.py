a63be2fc52fd6d4cb78a18b9f4ed6730e73318a9
BYJiang <jiangboyuan@gmail.com>
2026-03-01 01:35:34 -0800

    Add address checksum conversion before contract creation

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -35,6 +35,7 @@ def connect_with_middleware(contract_json):
 	# and https://web3py.readthedocs.io/en/stable/web3.contract.html
 	contract = 0
 	w3.middleware_onion.inject(ExtraDataToPOAMiddleware, layer=0)
+	address = w3.to_checksum_address(address)
 	contract= w3.eth.contract(address=address, abi=abi)
 	return w3, contract
 
